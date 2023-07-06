b220c0cd32fcf504f67c49cfe16eb0bfefd1faaf
kesavaprasad08 <kesav2661998@gmail.com>
2023-07-06 13:33:57 +0530

    Link to Developers

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function AboutUsPage() {
-    return <h1>The About us Page</h1>
-  }
+    return(
+    <Fragment>
+    <h1>The About us Page</h1>
+    <h2>Developers</h2>
+    <ul>
+      <li><Link href='/aboutus/1'>Yash</Link></li>
+      <li><Link href='/aboutus/2'>Vaibhav</Link></li>
+      <li><Link href='/aboutus/3'>Suresh</Link></li>
+    </ul>
+    </Fragment>
+    )  
+}
 
   export default AboutUsPage;
